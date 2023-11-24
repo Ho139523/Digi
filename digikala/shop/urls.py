73798bdc08cd63cffa0c837f436a157afcbd690a
Hussein <answereeee4@gmail.com>
2023-11-24 11:24:08 +0000

    login and logout is accomplished.

--- digikala/shop/urls.py
+++ digikala/shop/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import hello_world
+from .views import homepage, login_user, logout_user
 
 
 app_name='shop'
 urlpatterns = [
-    path('', hello_world, name='home'),
+    path('', homepage, name='home'),
+    path('login/', login_user,name='login'),
+    path('logout/', logout_user,name='logout'),
+   # path('signup/', signup_user,name='singup'),
 ]
--- digikala/shop/views.py
+++ digikala/shop/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product_model
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-def hello_world(request):
+def homepage(request):
     context={'product': Product_model.objects.all()}
     return render(request, 'shop/home.html', context=context)
+
+
+def login_user(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('با موفقیت وارد شدید.'))
+            return redirect('shop:home')
+        else:
+            messages.success(request, ('ورود شما با مشکلی رو به رو شده است.'))
+            return redirect('shop:login')
+    else:
+        return render(request, 'shop/signin.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('با موفقیت خارج شدید.'))
+    return redirect('shop:home')
+    #context={}
+    #return render(request, 'shop/signout',)
